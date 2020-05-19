e064e392023aa03a328697fee356cc67578e1eda
장성구 <ggrty10@gmail.com>
2020-05-19 13:59:45 +0900

    Initial Commit

---
+++ NowTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NowTalk
+//
+//  Created by 장성구 on 2020/05/19.
+//  Copyright © 2020 com.sg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
