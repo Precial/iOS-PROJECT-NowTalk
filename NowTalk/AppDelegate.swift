1695600573dc3a6a1d0361755a9d9d63e11f50f1
장성구 <ggrty10@gmail.com>
2020-05-24 23:12:09 +0900

    네비게이션 바 색상 설정

--- NowTalk/AppDelegate.swift
+++ NowTalk/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        UINavigationBar.appearance().barTintColor = UIColor.white // 네이게이션 바 색상 섫정
+
+     
         FirebaseApp.configure()
         
         return true
