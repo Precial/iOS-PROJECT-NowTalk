e2931bf74d90576934a7c7f76decdb4e6ce9f202
장성구 <ggrty10@gmail.com>
2020-05-28 11:13:45 +0900

    [더보기] collectionView 구성

--- NowTalk/TabBar/AccountViewController.swift
+++ NowTalk/TabBar/AccountViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 import Firebase
 
 
-class AccountViewController: UIViewController {
+class AccountViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+ 
+    
 
     
     @IBOutlet weak var conditionsCommentButton: UIButton!
+    
+    
+    let viewModel = BountyViewModel()
+    
+    
+    //performSegue 가 실행되기전 준비하는 과정
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // DetailViewController 데이터 wnrl
+        
+        if segue.identifier == "showDetail" {
+            let vc = segue.destination as? DetailViewController
+            if let index = sender as? Int{
+
+                let bountyInfo = viewModel.bountyInfo(at: index)
+
+                vc?.viewModel.update(model: bountyInfo)
+           }
+        
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,5 +90,86 @@ class AccountViewController: UIViewController {
     }
     
     
+     // 몇개를 보여줄까요
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numOfBountyInfoList
+    }
+    
+    // 셀은 어떻게 표현할꺼야?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
+        }
+            
+            
+            let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
+            cell.update(info: bountyInfo)
+            cell.update(info: bountyInfo)
+            return cell
+    }
+    
+    // UICollectionViewDelegate,
+    // 셀이 클릭되었을떄 어쩔까요
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("--> \(indexPath.item)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
+    
 
+    // UICollectionViewDelegateFlowLayout
+    // cell size를 계산할거다 ( 목표: 다양한 디바이스에서 일관적인 디자인을 보여주기 위해)
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let itemSpacing: CGFloat = 10
+//        let textAreaHeigt: CGFloat = 65
+//
+//        let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2
+//        let height: CGFloat = width * 10/7 + textAreaHeigt
+//
+//        return CGSize(width: width, height: height)
+//    }
+    
+    
+
+}
+
+class BountyViewModel {
+    let bountyInfoList: [BountyInfo] = [
+            BountyInfo(name: "brook", bounty: 33000000),
+            BountyInfo(name: "chopper", bounty: 50),
+            BountyInfo(name: "franky", bounty: 44000000),
+            BountyInfo(name: "luffy", bounty: 300000000),
+            BountyInfo(name: "nami", bounty: 16000000),
+            BountyInfo(name: "robin", bounty: 80000000),
+       ]
+    
+    var sortedList: [BountyInfo] {
+           let sortedList = bountyInfoList.sorted { prev, next  in
+               return prev.bounty > next.bounty
+           }
+           
+           return sortedList
+       }
+    
+    var numOfBountyInfoList: Int {
+        return bountyInfoList.count
+    }
+    
+    func bountyInfo(at index: Int) -> BountyInfo {
+        return sortedList[index]
+    }
+}
+
+class GridCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    func update(info: BountyInfo) {
+            imgView.image =  info.image
+            nameLabel.text = info.name
+            bountyLabel.text = "\(info.bounty)"
+    }
+    
 }
